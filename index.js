a47428f1cbd093be905680961f1fe1ebe267c539
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-04-28 18:53:13 -0500

    Se agregaron las lineas para utilizar ExplorerServices

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService");
 
 const explorers = Reader.readJsonFile("explorers.json"); 
 console.log(explorers)
+
+// Aplicación del ExplorerService sobre la lista de explorers
+const filterByMission = ExplorerService.filterByMission(explorers, "java");
+console.log(filterByMission)
+
+const getAmountOfExplorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "java");
+console.log(getAmountOfExplorersByMission);
+
+const getExplorersUsernamesByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+console.log(getExplorersUsernamesByMission)
