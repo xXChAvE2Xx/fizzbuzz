1422d4dc4c42a51446acf27332d30393088ab07b
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-04-28 20:15:24 -0500

    Se agregaron las validaciones para utilizar Fizzbuzz Service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
 const explorers = Reader.readJsonFile("explorers.json"); 
 console.log(explorers)
@@ -13,3 +14,16 @@ console.log(getAmountOfExplorersByMission);
 
 const getExplorersUsernamesByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 console.log(getExplorersUsernamesByMission)
+
+
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
