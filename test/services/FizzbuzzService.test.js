c98f2055636f12d54b5fbd5426402433528bd2b1
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-04-28 20:22:50 -0500

    Se agregaron Test para BUZZ y FIZZBUZZ

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -8,4 +8,21 @@ describe("Pruebas unitarias para FizzbuzzService", () =>{
 
         expect(FIZZ.trick).toBe("FIZZ");
     });
+
+    test("If the score numbers is divisible by 5, I need a new propery called trick, and the value assigned is BUZZ", () =>{
+        const explorer = {name: "Explorer2", score: 5}
+
+        const BUZZ = FizzbuzzService.applyValidationInExplorer(explorer);
+
+        expect(BUZZ.trick).toBe("BUZZ");
+    })
+
+
+    test("If the score numbers is divisible by 5 and 3, I need a new propery called trick, and the value assigned is FIZZBUZZ", () =>{
+        const explorer = {name: "Explorer3", score: 15}
+
+        const FIZZBUZZ = FizzbuzzService.applyValidationInExplorer(explorer);
+
+        expect(FIZZBUZZ.trick).toBe("FIZZBUZZ");
+    })
 })
